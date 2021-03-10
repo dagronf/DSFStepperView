63fcdfbd4bbbfd3f4fccc69a6bcbd5fdc2e3f309
Darren Ford <dford_au-reg@yahoo.com>
2021-03-10 18:40:25 +1100

    Changed to not use autolayout for simplicity with SwiftUI

--- Sources/DSFStepperView/DSFStepperView+iOS.swift
+++ Sources/DSFStepperView/DSFStepperView+iOS.swift
@@ -34,6 +34,10 @@ public class DSFStepperView: UIView {
 	static let borderStrokeDefault = UIColor.lightGray.withAlphaComponent(0.4)
 	static let borderFillDefault = UIColor.lightGray.withAlphaComponent(0.2)
 
+	// The CGFloat value that indicates 'empty' for the control
+	static let OptionalIndicatorValue = CGFloat.greatestFiniteMagnitude
+
+
 	/// The (optional) callback delegate
 	@objc public var delegate: DSFStepperViewDelegateProtocol? {
 		didSet {
@@ -56,7 +60,7 @@ public class DSFStepperView: UIView {
 	@IBInspectable public var allowsKeyboardInput: Bool = false
 
 	/// Placeholder text to display if the field is empty
-	@IBInspectable public var placeholder: String? = "Default" {
+	@IBInspectable public var placeholder: String? = "Inherited" {
 		didSet {
 			if self.allowsEmpty {
 				self.editField.placeholder = self.placeholder
@@ -64,26 +68,29 @@ public class DSFStepperView: UIView {
 		}
 	}
 
-
+	/// The color to draw the border
 	@IBInspectable public var borderColor: UIColor = DSFStepperView.borderStrokeDefault {
 		didSet {
 			self.layer.borderColor = self.borderColor.cgColor
 		}
 	}
 
+	/// The color to draw the background
 	@IBInspectable public var borderBackground: UIColor = DSFStepperView.borderFillDefault {
 		didSet {
 			self.layer.backgroundColor = self.borderBackground.cgColor
 		}
 	}
 
-	@IBInspectable public var textColor: UIColor = UIColor.label {
+	/// The color to draw the text
+	@IBInspectable public var foregroundColor: UIColor = UIColor.label {
 		didSet {
-			self.editField.textColor = self.textColor
+			self.editField.textColor = self.foregroundColor
 			self.updateAvailability()
 		}
 	}
 
+	/// The color to draw the indicator
 	@IBInspectable public var indicatorColor: UIColor = UIColor.systemBlue {
 		didSet {
 			self.indicatorLayer.backgroundColor = self.indicatorColor.cgColor
@@ -93,33 +100,37 @@ public class DSFStepperView: UIView {
 	/// The minimum allowable value in the control
 	@IBInspectable public var minimum: CGFloat = -CGFloat.greatestFiniteMagnitude {
 		didSet {
-			if let floatVal = self._floatValue, floatVal < self.minimum {
-				self._floatValue = self.minimum
+			if let val = self.cgFloatValue,
+				val < self.minimum {
+				self.floatValue = self.minimum.numberValue
 			}
+			self.floatValueUpdated()
 		}
 	}
 
 	/// The maximum allowable value in the control
 	@IBInspectable public var maximum: CGFloat = CGFloat.greatestFiniteMagnitude {
 		didSet {
-			if let floatVal = self._floatValue, floatVal > self.maximum {
-				self._floatValue = self.maximum
+			if let val = self.cgFloatValue,
+				val > self.maximum {
+				self.floatValue = self.maximum.numberValue
 			}
+			self.floatValueUpdated()
 		}
 	}
-
-	private func contains(_ value: CGFloat) -> Bool {
-		return value >= self.minimum && value <= self.maximum
-	}
-
 
 	/// How much to increment/decrement the value when pressing the -/+ buttons
 	@IBInspectable public var increment: CGFloat = 1
 
 	/// The initial value to be displayed in the control
-	@IBInspectable public var initialValue: NSNumber? = NSNumber(value: 0) {
+	@IBInspectable public var initialValue: CGFloat = 0 {
 		didSet {
-			self.floatValue = self.initialValue
+			if self.allowsEmpty && self.initialValue == DSFStepperView.OptionalIndicatorValue {
+				self.floatValue = nil
+			}
+			else {
+				self.floatValue = self.initialValue.numberValue
+			}
 		}
 	}
 
@@ -138,52 +149,52 @@ public class DSFStepperView: UIView {
 	}
 
 	private var fractionalPosition: CGFloat = 0
+	private var _previousValue: NSNumber?
 
-	private var _previousValue: CGFloat?
 
-	private var _floatValue: CGFloat? {
+	fileprivate var cgFloatValue: CGFloat? {
+		return floatValue?.cgFloatValue
+	}
+
+	@objc dynamic public var floatValue: NSNumber? {
 		didSet {
-			if self.allowsEmpty, self._floatValue == nil {
-				self.editField.text = nil
-			}
-			else if let value = self._floatValue,
-					  let str = self.numberFormatter.string(from: value.numberValue)
-			{
-				self.editField.text = str
-			}
-			else {
-				self.editField.text = nil
-			}
-
-			self.updateAvailability()
-
-			self.rebuild()
-
-			self.delegate?.stepperView(self, didChangeValueTo: self.floatValue)
+			self.floatValueUpdated()
 		}
 	}
 
-	@objc public var floatValue: NSNumber? {
-		get {
-			return self._floatValue?.numberValue ?? nil
-		}
-		set {
-			if let nv = newValue?.floatValue {
-				self._floatValue = min(self.maximum, max(self.minimum, CGFloat(nv)))
+	private func floatValueUpdated() {
+		guard let cgv = self.floatValue?.cgFloatValue else {
+			if self.allowsEmpty {
+				self.editField.text = nil
+				return
 			}
-			else {
-				self._floatValue = nil
-			}
+//			fatalError("Cannot set nil value when 'allowsEmpty' is false")
+			return
 		}
+
+		let nv = min(self.maximum, max(self.minimum, cgv)).numberValue
+		if let str = self.numberFormatter.string(from: nv) {
+			self.editField.text = str
+		}
+		else {
+			//fatalError("Cannot set value for numberformatter")
+		}
+
+		self.updateAvailability()
+		self.updateIndicatorBar()
+
+		// Notify the delegate of the change
+		self.delegate?.stepperView(self, didChangeValueTo: self.floatValue)
+
 	}
 
 	private func updateAvailability() {
 		let canIncrease: Bool
 		let canDecrease: Bool
 
-		if let f = self._floatValue {
-			canIncrease = self.isEnabled ? f < self.maximum : false
-			canDecrease = self.isEnabled ? f > self.minimum : false
+		if let val = self.cgFloatValue {
+			canIncrease = self.isEnabled ? val < self.maximum : false
+			canDecrease = self.isEnabled ? val > self.minimum : false
 		}
 		else {
 			canIncrease = false
@@ -198,10 +209,16 @@ public class DSFStepperView: UIView {
 
 		self.editField.isEnabled = self.isEnabled
 
-		self.editField.textColor = self.isEnabled ? self.textColor : .lightGray
+		self.editField.textColor = self.isEnabled ? self.foregroundColor : .lightGray
 	}
 
-	static let defaultFormatter: NumberFormatter = {
+	// Is 'value' contained within the min and max ranges?
+	private func contains(_ value: CGFloat) -> Bool {
+		return value >= self.minimum && value <= self.maximum
+	}
+
+	// A default formatter that deals only with decimal values (no float)
+	private static let defaultFormatter: NumberFormatter = {
 		let f = NumberFormatter()
 		f.allowsFloats = false
 		return f
@@ -212,7 +229,7 @@ public class DSFStepperView: UIView {
 	/// A number formatter for display and validation within the control (optional)
 	public var numberFormatter: NumberFormatter = DSFStepperView.defaultFormatter {
 		didSet {
-			self._floatValue = self.floatValue?.cgFloatValue
+			self.floatValueUpdated()
 		}
 	}
 
@@ -253,18 +270,18 @@ public class DSFStepperView: UIView {
 		CATransaction.begin()
 		CATransaction.setDisableActions(true)
 		self.maskLayer.path = CGPath(roundedRect: self.bounds, cornerWidth: 6, cornerHeight: 6, transform: nil)
-		self.rebuild()
+		self.updateIndicatorBar()
 		CATransaction.commit()
 	}
 
-	func rebuild() {
+	func updateIndicatorBar() {
 		guard self.minimum > -CGFloat.greatestFiniteMagnitude || self.maximum < CGFloat.greatestFiniteMagnitude else {
 			self.fractionalPosition = 0
 			self.indicatorLayer.frame = .zero
 			return
 		}
 
-		if let val = self._floatValue {
+		if let val = self.cgFloatValue {
 			self.fractionalPosition = abs(val - self.minimum) / abs(self.maximum - self.minimum)
 		}
 		else {
@@ -303,6 +320,8 @@ public class DSFStepperView: UIView {
 																			metrics: nil, views: ["stack": self.stack]))
 
 		self.minusButton.translatesAutoresizingMaskIntoConstraints = false
+		self.minusButton.isAccessibilityElement = true
+		self.minusButton.accessibilityLabel = "Decrement stepper"
 		self.minusButton.setImage(UIImage(systemName: "minus"), for: .normal)
 		self.minusButton.addConstraint(NSLayoutConstraint(item: self.minusButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50))
 		self.minusButton.tintColor = self.editField.textColor
@@ -316,6 +335,8 @@ public class DSFStepperView: UIView {
 
 		self.plusButton.setImage(UIImage(systemName: "plus"), for: .normal)
 		self.plusButton.translatesAutoresizingMaskIntoConstraints = false
+		self.plusButton.isAccessibilityElement = true
+		self.plusButton.accessibilityLabel = "Increment stepper"
 		self.plusButton.addConstraint(NSLayoutConstraint(item: self.plusButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50))
 		self.plusButton.tintColor = self.editField.textColor
 
@@ -327,39 +348,77 @@ public class DSFStepperView: UIView {
 		self.stack.addArrangedSubview(self.editField)
 		self.stack.addArrangedSubview(self.plusButton)
 
-		self.floatValue = self.initialValue
+		self.setupAccessibility()
+
+		if self.allowsEmpty && self.initialValue == DSFStepperView.OptionalIndicatorValue {
+			self.floatValue = nil
+		}
+		else {
+			self.floatValue = self.initialValue.numberValue
+		}
 
 		self.updateAvailability()
-
-		self.rebuild()
+		self.updateIndicatorBar()
 	}
 
 	@objc func performIncrement() {
-		guard let val = self._floatValue else {
-			return
+		if let val = self.cgFloatValue {
+			let newVal = val + self.increment
+			self.floatValue = max(self.minimum, min(self.maximum, newVal)).numberValue
 		}
-
-		let newVal = val + self.increment
-		self.floatValue = max(self.minimum, min(self.maximum, newVal)).numberValue
 	}
 
 	@objc func performDecrement() {
-		guard let val = self._floatValue else {
-			return
+		if let val = self.cgFloatValue {
+			let newVal = val - self.increment
+			self.floatValue = max(self.minimum, min(self.maximum, newVal)).numberValue
 		}
+	}
+}
 
-		let newVal = val - self.increment
-		self.floatValue = max(self.minimum, min(self.maximum, newVal)).numberValue
+extension DSFStepperView {
+
+	public override var accessibilityValue: String? {
+		get {
+			if let f = self.floatValue {
+				return self.numberFormatter.string(from: f)
+			}
+			else {
+				return "empty"
+			}
+		}
+		set {
+			if let n = newValue,
+				let cg = self.numberFormatter.number(from: n) {
+				self.floatValue = cg
+			}
+			else {
+				self.floatValue = NSNumber(value: 0)
+			}
+		}
+	}
+
+	func setupAccessibility() {
+		self.isAccessibilityElement = true
+		self.accessibilityLabel = "Stepper"
+	}
+
+	public override func accessibilityIncrement() {
+		self.performIncrement()
+	}
+
+	public override func accessibilityDecrement() {
+		self.performDecrement()
 	}
 }
 
 extension DSFStepperView: UITextFieldDelegate {
-	public func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+	public func textFieldShouldBeginEditing(_: UITextField) -> Bool {
 		return self.allowsKeyboardInput
 	}
 
-	public func textFieldDidBeginEditing(_ textField: UITextField) {
-		self._previousValue = self._floatValue
+	public func textFieldDidBeginEditing(_: UITextField) {
+		self._previousValue = self.floatValue
 	}
 
 	public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
@@ -378,29 +437,20 @@ extension DSFStepperView: UITextFieldDelegate {
 
 	public func textFieldDidEndEditing(_ textField: UITextField) {
 
-		if textField.text?.isEmpty ?? true {
-			if self.allowsEmpty {
-				self.floatValue = nil
-			}
-			else {
-				self._floatValue = self._previousValue
-			}
-		}
-
-		if self.allowsEmpty, textField.text?.isEmpty ?? false {
+		if self.allowsEmpty, textField.text?.isEmpty ?? true {
 			self.floatValue = nil
 			return
 		}
 
-		if let t = textField.text,
-			let s = self.numberFormatter.number(from: t)
-		{
+		if let t = textField.text, let s = self.numberFormatter.number(from: t) {
 			self.floatValue = s
+		}
+		else {
+			textField.text = self.numberFormatter.string(for: self.floatValue)
 		}
 	}
 
 	public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-
 		var text = textField.text ?? ""
 
 		guard let ran = Range(range, in: text) else {
@@ -409,12 +459,13 @@ extension DSFStepperView: UITextFieldDelegate {
 
 		text = text.replacingCharacters(in: ran, with: string)
 
-		if self.allowsEmpty && text.isEmpty {
+		if self.allowsEmpty, text.isEmpty {
 			return true
 		}
 
 		if let val = self.numberFormatter.number(from: text),
-			self.contains(val.cgFloatValue) {
+			self.contains(val.cgFloatValue)
+		{
 			return true
 		}
 
